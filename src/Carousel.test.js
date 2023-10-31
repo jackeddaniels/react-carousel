c70592d757ebfa60f6428d934424404cc4cc272b
John Flores <johnflores1991@hotmail.com>
2023-10-31 18:32:45 -0500

    added tests for Card and Carousel

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -2,6 +2,22 @@ import { render, fireEvent } from "@testing-library/react";
 import Carousel from "./Carousel";
 import TEST_IMAGES from "./_testCommon.js";
 
+it("renders without crashing", function () {
+  // smoke test
+  render(<Carousel
+    photos={TEST_IMAGES}
+    title="images for testing"
+  />);
+});
+
+it("matches snapshot", function () {
+  const { container } = render(<Carousel
+    photos={TEST_IMAGES}
+    title="images for testing"
+  />);
+  expect(container).toMatchSnapshot();
+});
+
 it("works when you click on the right arrow", function() {
   const { container } = render(
     <Carousel
